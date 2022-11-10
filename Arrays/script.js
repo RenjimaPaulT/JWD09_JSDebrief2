0ee14fa857ecee8bf7ea47c6e20aab8c17cb0bd9
RenjiT <renjis33@gmail.com>
2022-11-10 22:37:18 +1100

    file changed

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,8 +1,35 @@
 let drivers = ['Sam','Peter','Jason','Adam','Jack']
 function resetDrivers()
 {
-drivers = ['Jim','Jerry'];
+drivers = ['Jim','Jerry','John'];
+console.log(drivers);
+}
+function appendArray()
+{
+    drivers.push('Jimmy');
+    console.log(drivers);
+}
+function prependArray()
+{
+    drivers.unshift('Jacob');
+    console.log(drivers);
+}
+function removeLast()
+{
+    drivers.pop();
+    console.log(drivers);
+}
+function removeFirst()
+{
+    drivers.shift();
+    console.log(drivers);
 }
 resetDrivers();
-
-console.log(drivers);
+appendArray();
+resetDrivers();
+prependArray();
+resetDrivers();
+removeLast();
+resetDrivers();
+removeFirst();
+resetDrivers();
